3524a9a8b35e104852085c4d30ab9ddefaf4ee29
Yu Shi <yushiys95@gmail.com>
2021-03-17 22:26:04 -0400

    commit tree 2d3699f86f18420f14d6b2f6ea9d86f29b2fac27

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+//The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
+
